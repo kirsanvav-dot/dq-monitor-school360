d6609da48901fc6cdcada5ce3b7371ccc009e26d
TimonMaloy <timon.maloy@gmail.com>
2026-05-17 09:54:32 +0300

    separation of error entities and work with them

---
+++ dq_monitor/src/constant_issue.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+#FIXME обоснование использования таких классов: другие части приложения
+#будут взаимодействовать с данными ошибками и чтобы их определять и не 
+#иметь проблем с опечатками мы используем переменные как константы
+class DQDimension(str, Enum):
+    "класс констант измерений"
+    COMPLETENESS = "Completeness"
+    VALIDITY = "Validity"
+    CONSISTENCY = "Consistency"
+    UNIQUENESS = "Uniqueness"
+
+class IssueType(Enum):
+    """
+    Класс констант типов ошибок.
+    Значение (value) — это кортеж: (имя_для_метода, описание_для_ui, измерение)
+    """
+    # Пример создания:
+    BAD_FORMAT_DATE = ("bad_format_date", "Неверный формат данных времени", DQDimension.VALIDITY)
+
+    @property
+    def method_name(self) -> str:
+        """Служебное имя, используется для поиска метода (например, 'bad_format_date')."""
+        return self.value[0]
+        
+    @property
+    def description(self) -> str:
+        """Человекочитаемое описание для UI."""
+        return self.value[1]
+        
+    @property
+    def dimension(self) -> DQDimension:
+        """Тип измерения качества."""
+        return self.value[2]
+
+#TODO сами типы определяются DQ
--- dq_monitor/src/profiler.py
+++ dq_monitor/src/profiler.py
@@ -64,45 +64,8 @@
 """
 from typing import Optional, List
 from dataclasses import dataclass
-from enum import Enum
+from src.constant_issue import IssueType, DQDimension
 import pandas as pd
-
-#FIXME обоснование использования таких классов: другие части приложения
-#будут взаимодействовать с данными ошибками и чтобы их определять и не 
-#иметь проблем с опечатками мы используем переменные как константы
-class DQDimension(str, Enum):
-    "класс констант измерений"
-    COMPLETENESS = "Completeness"
-    VALIDITY = "Validity"
-    CONSISTENCY = "Consistency"
-    UNIQUENESS = "Uniqueness"
-
-class IssueType(Enum):
-    """
-    Класс констант типов ошибок.
-    Значение (value) — это кортеж: (имя_для_метода, описание_для_ui, измерение)
-    """
-    # Пример создания:
-    BAD_FORMAT_DATE = ("bad_format_date", "Неверный формат данных времени", DQDimension.VALIDITY)
-
-    @property
-    def method_name(self) -> str:
-        """Служебное имя, используется для поиска метода (например, 'bad_format_date')."""
-        return self.value[0]
-        
-    @property
-    def description(self) -> str:
-        """Человекочитаемое описание для UI."""
-        return self.value[1]
-        
-    @property
-    def dimension(self) -> DQDimension:
-        """Тип измерения качества."""
-        return self.value[2]
-
-#TODO сами типы определяются DQ
-
-
 
 ALL = "all" #служебное определение для column, если нет четкого столбца
 
