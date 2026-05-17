7d6a783c461b902724ae06254d4cdaf4dd9f5948
alekseyka <aleksey.isk@gmail.com>
2026-05-17 11:45:24 +0300

    Fix tests

--- dq_monitor/tests/test_profiler.py
+++ dq_monitor/tests/test_profiler.py
@@ -10,4 +10,4 @@ def test_profiler_returns_disired_df(small_dirty_df):
 def test_DQIssue_has_rows_affected_in_list():
     assert DQIssue.__annotations__["rows_affected"] == "List[int]"
 
-def 
+# def
